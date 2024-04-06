e54d6b972991810310a45e19c32f876f66356e87
DJL1n <djl1n_private@163.com>
2024-04-06 14:34:32 +0800

    2024.04.06

--- 算法/双指针/三数之和.py
+++ 算法/双指针/三数之和.py
@@ -2,25 +2,29 @@
 作者：legionb
 日期：2024年04月05日
 """
-nums=list(map(int,input().split()))
-n=len(nums)
-res=[]
+# 对撞指针
+# 基本思路，调用双指针减少循环数
+nums = list(map(int, input().split()))
+n = len(nums)
+res = []
 nums.sort()
-pre=float("inf")
+pre = float("inf")  # 第一次去重，存储上一个的第一个数，去除第一个数相同的情况
 for i in range(n):
-    left=i+1
-    right=n-1
-    while left<right:
-        total=nums[i]+nums[left]+nums[right]
-        if total==0:
-            if pre==nums[left]:
-                continue
-            res.append([nums[i],nums[left],nums[right]])
-            pre=nums[left]
-            break
-        elif total>0:
-            right-=1
+    if pre == nums[i]:
+        continue
+    left = i + 1
+    right = n - 1
+    while left < right:
+        total = nums[i] + nums[left] + nums[right]
+        if total == 0:
+            res.append([nums[i], nums[left], nums[right]])
+            temp = nums[left]  # 第二次去重，去除第一个和第二个数都相同的情况
+            while left < right and temp == nums[left]:  # 并且继续查找是否有其他的可能
+                left += 1
+        elif total > 0:
+            right -= 1
         else:
-            left+=1
+            left += 1
         # return res
+    pre = nums[i]
 print(res)
---
+++ 算法/双指针/链表的中间节点.py
@@ -0,0 +1,19 @@
+"""
+作者：legionb
+日期：2024年04月06日
+"""
+#快慢指针
+#当快指针到结尾时，慢指针的位置就是中间节点
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head):
+        fast,slow=head,head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+        return slow
+
