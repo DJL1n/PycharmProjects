869e03c5f91526dd1713547426556a6ddc869142
DJL1n <djl1n_private@163.com>
2024-04-09 10:42:44 +0800

    2024.04.08

--- 算法/蓝桥其他/子树的大小.py
+++ 算法/蓝桥其他/子树的大小.py
@@ -4,17 +4,20 @@
 """
 def ChildtreeSize(n,m,k):
     if k>n:
-        return
-    global count
-    count+=1
-    for i in range()
+        return 0
+    # global count
+    count=1
+    for i in range(-1,m-1):
+        count+=ChildtreeSize(n,m,m*k-i)
+    return count
 
 N=int(input())
-lst=[0]+[1]*10**9
-n=len(lst)
-for i in range(1,n):
-    lst[i]+=lst[i-1]
+# lst=[0]+[1]*10**9
+# n=len(lst)
+# for i in range(1,n):
+#     lst[i]+=lst[i-1]
 for i in range(N):
-    count=0
+    # count=0
     n,m,k=map(int,input().split())
-    ChildtreeSize(n,m,k)
+    print(ChildtreeSize(n,m,k))
+
