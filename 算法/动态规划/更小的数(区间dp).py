74c0c9c99bb19ee5fb828d7586af57257b034f49
DJL1n <djl1n_private@163.com>
2024-04-11 21:09:33 +0800

    2024.04.11

--- 算法/动态规划/更小的数(区间dp).py
+++ 算法/动态规划/更小的数(区间dp).py
@@ -19,7 +19,6 @@
 # print(res)
 
 
-
 # s = input()
 # ans = 0
 # for i in range(len(s)):
@@ -37,23 +36,22 @@
 #
 # print(ans)
 
-s=[int(i) for i in input()]
+s = [int(i) for i in input()]
 # print(s)
-n=len(s)
-res=0
-dp=[[0]*n for _ in range(n+1)]
-for i in range(n-1):
-    if s[i]>s[i+1]:
-        dp[2][i]=1
-        res+=1
-for i in range(n-2):
-    if s[i]>s[i+2]:
-        dp[3][i]=1
-        res+=1
-for i in range(4,n+1):
-    for j in range(n-i+1):
-        if s[j]>s[j+i-1] or s[j]==s[j+i-1] and dp[i-2][j+1]==1:
-            dp[i][j]=1
-            res+=1
+n = len(s)
+res = 0
+dp = [[0] * n for _ in range(n + 1)]
+for i in range(n - 1):
+    if s[i] > s[i + 1]:
+        dp[2][i] = 1
+        res += 1
+for i in range(n - 2):
+    if s[i] > s[i + 2]:
+        dp[3][i] = 1
+        res += 1
+for i in range(4, n + 1):
+    for j in range(n - i + 1):
+        if s[j] > s[j + i - 1] or s[j] == s[j + i - 1] and dp[i - 2][j + 1] == 1:
+            dp[i][j] = 1
+            res += 1
 print(res)
-
