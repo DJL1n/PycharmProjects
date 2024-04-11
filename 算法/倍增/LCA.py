2a780e5ed22fa157576bf8a5aa9f6e9c16597afd
DJL1n <djl1n_private@163.com>
2024-04-11 22:15:45 +0800

    2024.04.11

---
+++ 算法/倍增/LCA.py
@@ -0,0 +1,12 @@
+"""
+作者：legionb
+日期：2024年04月11日
+"""
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root or root == p or root == q: return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left: return right
+        if not right: return left
+        return root
