7bff2d736cad656707811446746576070f0bd818
DJL1n <djl1n_private@163.com>
2024-04-30 19:08:24 +0800

    2024.04.14

--- 大创项目/申报前的实验准备/gpt2的分词器.py
+++ 大创项目/申报前的实验准备/gpt2的分词器.py
@@ -6,23 +6,15 @@ import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 # 加载预训练的GPT-2模型和分词器
-model_name = "gpt2-medium"  # 您也可以选择其他规模的模型，例如"gpt2"或"gpt2-large"
+model_name = "gpt2-medium"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-# 设定生成文本的长度
 max_length = 100
-
-# 要生成的初始文本
 prompt_text = "Once upon a time, there was a king who ruled over a vast kingdom. "
 
-# 将文本转换为token
 input_ids = tokenizer.encode(prompt_text, return_tensors="pt")
-
-# 使用模型生成新文本
 output = model.generate(input_ids, max_length=max_length, num_return_sequences=1, do_sample=True)
-
-# 将生成的token转换回文本
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
 print("Generated Text:\n", generated_text)
