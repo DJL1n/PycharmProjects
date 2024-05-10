58ee3304981ccbda9e9536dba8365f444406a43e
DJL1n <djl1n_private@163.com>
2024-05-10 22:56:36 +0800

    2024.5.10

--- 大创项目/申报前的实验准备/手动实现Bert结构.py
+++ 大创项目/申报前的实验准备/手动实现Bert结构.py
@@ -7,12 +7,13 @@ import math
 import numpy as np
 from transformers import BertModel
 
-
 bert = BertModel.from_pretrained(r"E:\临时\大赛\大创\bert-base-chinese", return_dict=False)
 state_dict = bert.state_dict()
 bert.eval()
-x = np.array([2450, 15486, 15167, 2110]) #通过vocab对应输入：深度学习
-torch_x = torch.LongTensor([x])  #pytorch形式输入
+x = np.array([2450, 15486, 15167, 2110])  # 通过vocab对应输入：深度学习
+torch_x = torch.LongTensor([x])  # pytorch形式输入
+
+
 # seqence_output, pooler_output = bert(torch_x)
 # print(seqence_output.shape, pooler_output.shape)
 # print(seqence_output, pooler_output)
@@ -20,16 +21,18 @@ torch_x = torch.LongTensor([x])  #pytorch形式输入
 # print(bert.state_dict().keys())  #查看所有的权值矩阵名称
 
 
-#softmax归一化
+# softmax归一化
 def softmax(x):
-    return np.exp(x)/np.sum(np.exp(x), axis=-1, keepdims=True)
+    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
 
-#gelu激活函数
+
+# gelu激活函数
 def gelu(x):
     return 0.5 * x * (1 + np.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * np.power(x, 3))))
 
+
 class DiyBert:
-    #将预训练好的整个权重字典输入进来
+    # 将预训练好的整个权重字典输入进来
     def __init__(self, state_dict):
         self.num_attention_heads = 12
         self.hidden_size = 768
@@ -37,14 +40,14 @@ class DiyBert:
         self.load_weights(state_dict)
 
     def load_weights(self, state_dict):
-        #embedding部分
+        # embedding部分
         self.word_embeddings = state_dict["embeddings.word_embeddings.weight"].numpy()
         self.position_embeddings = state_dict["embeddings.position_embeddings.weight"].numpy()
         self.token_type_embeddings = state_dict["embeddings.token_type_embeddings.weight"].numpy()
         self.embeddings_layer_norm_weight = state_dict["embeddings.LayerNorm.weight"].numpy()
         self.embeddings_layer_norm_bias = state_dict["embeddings.LayerNorm.bias"].numpy()
         self.transformer_weights = []
-        #transformer部分，有多层
+        # transformer部分，有多层
         for i in range(self.num_layers):
             q_w = state_dict["encoder.layer.%d.attention.self.query.weight" % i].numpy()
             q_b = state_dict["encoder.layer.%d.attention.self.query.bias" % i].numpy()
@@ -62,64 +65,66 @@ class DiyBert:
             output_bias = state_dict["encoder.layer.%d.output.dense.bias" % i].numpy()
             ff_layer_norm_w = state_dict["encoder.layer.%d.output.LayerNorm.weight" % i].numpy()
             ff_layer_norm_b = state_dict["encoder.layer.%d.output.LayerNorm.bias" % i].numpy()
-            self.transformer_weights.append([q_w, q_b, k_w, k_b, v_w, v_b, attention_output_weight, attention_output_bias,
-                                             attention_layer_norm_w, attention_layer_norm_b, intermediate_weight, intermediate_bias,
-                                             output_weight, output_bias, ff_layer_norm_w, ff_layer_norm_b])
-        #pooler层
+            self.transformer_weights.append(
+                [q_w, q_b, k_w, k_b, v_w, v_b, attention_output_weight, attention_output_bias,
+                 attention_layer_norm_w, attention_layer_norm_b, intermediate_weight, intermediate_bias,
+                 output_weight, output_bias, ff_layer_norm_w, ff_layer_norm_b])
+        # pooler层
         self.pooler_dense_weight = state_dict["pooler.dense.weight"].numpy()
         self.pooler_dense_bias = state_dict["pooler.dense.bias"].numpy()
 
-
-    #bert embedding，使用3层叠加，在经过一个embedding层
+    # bert embedding，使用3层叠加，在经过一个embedding层
     def embedding_forward(self, x):
         # x.shape = [max_len]
         we = self.get_embedding(self.word_embeddings, x)  # shpae: [max_len, hidden_size]
         # position embeding的输入 [0, 1, 2, 3]
-        pe = self.get_embedding(self.position_embeddings, np.array(list(range(len(x)))))  # shpae: [max_len, hidden_size]
+        pe = self.get_embedding(self.position_embeddings,
+                                np.array(list(range(len(x)))))  # shpae: [max_len, hidden_size]
         # token type embedding,单输入的情况下为[0, 0, 0, 0]
         te = self.get_embedding(self.token_type_embeddings, np.array([0] * len(x)))  # shpae: [max_len, hidden_size]
         embedding = we + pe + te
         # 加和后有一个归一化层
-        embedding = self.layer_norm(embedding, self.embeddings_layer_norm_weight, self.embeddings_layer_norm_bias)  # shpae: [max_len, hidden_size]
+        embedding = self.layer_norm(embedding, self.embeddings_layer_norm_weight,
+                                    self.embeddings_layer_norm_bias)  # shpae: [max_len, hidden_size]
         return embedding
 
-    #embedding层实际上相当于按index索引，或理解为onehot输入乘以embedding矩阵
+    # embedding层实际上相当于按index索引，或理解为onehot输入乘以embedding矩阵
     def get_embedding(self, embedding_matrix, x):
         return np.array([embedding_matrix[index] for index in x])
 
-    #执行全部的transformer层计算
+    # 执行全部的transformer层计算
     def all_transformer_layer_forward(self, x):
         for i in range(self.num_layers):
             x = self.single_transformer_layer_forward(x, i)
         return x
 
-    #执行单层transformer层计算
+    # 执行单层transformer层计算
     def single_transformer_layer_forward(self, x, layer_index):
         weights = self.transformer_weights[layer_index]
-        #取出该层的参数，在实际中，这些参数都是随机初始化，之后进行预训练
+        # 取出该层的参数，在实际中，这些参数都是随机初始化，之后进行预训练
         q_w, q_b, \
-        k_w, k_b, \
-        v_w, v_b, \
-        attention_output_weight, attention_output_bias, \
-        attention_layer_norm_w, attention_layer_norm_b, \
-        intermediate_weight, intermediate_bias, \
-        output_weight, output_bias, \
-        ff_layer_norm_w, ff_layer_norm_b = weights
-        #self attention层
+            k_w, k_b, \
+            v_w, v_b, \
+            attention_output_weight, attention_output_bias, \
+            attention_layer_norm_w, attention_layer_norm_b, \
+            intermediate_weight, intermediate_bias, \
+            output_weight, output_bias, \
+            ff_layer_norm_w, ff_layer_norm_b = weights
+        # self attention层
         attention_output = self.self_attention(x,
-                                q_w, q_b,
-                                k_w, k_b,
-                                v_w, v_b,
-                                attention_output_weight, attention_output_bias,
-                                self.num_attention_heads,
-                                self.hidden_size)
-        #bn层，并使用了残差机制
+                                               q_w, q_b,
+                                               k_w, k_b,
+                                               v_w, v_b,
+                                               attention_output_weight, attention_output_bias,
+                                               self.num_attention_heads,
+                                               self.hidden_size)
+        # bn层，并使用了残差机制
         x = self.layer_norm(x + attention_output, attention_layer_norm_w, attention_layer_norm_b)
-        #feed forward层
+        # feed forward层
         feed_forward_x = self.feed_forward(x,
-                              intermediate_weight, intermediate_bias,
-                              output_weight, output_bias)
-        #bn层，并使用了残差机制
+                                           intermediate_weight, intermediate_bias,
+                                           output_weight, output_bias)
+        # bn层，并使用了残差机制
         x = self.layer_norm(x + feed_forward_x, ff_layer_norm_w, ff_layer_norm_b)
         return x
 
@@ -161,7 +166,7 @@ class DiyBert:
         attention = np.dot(qkv, attention_output_weight.T) + attention_output_bias
         return attention
 
-    #多头机制
+    # 多头机制
     def transpose_for_scores(self, x, attention_head_size, num_attention_heads):
         # hidden_size = 768  num_attent_heads = 12 attention_head_size = 64
         max_len, hidden_size = x.shape
@@ -169,7 +174,7 @@ class DiyBert:
         x = x.swapaxes(1, 0)  # output shape = [num_attention_heads, max_len, attention_head_size]
         return x
 
-    #前馈网络的计算
+    # 前馈网络的计算
     def feed_forward(self,
                      x,
                      intermediate_weight,  # intermediate_size, hidden_size
@@ -184,19 +189,19 @@ class DiyBert:
         x = np.dot(x, output_weight.T) + output_bias
         return x
 
-    #归一化层
+    # 归一化层
     def layer_norm(self, x, w, b):
         x = (x - np.mean(x, axis=1, keepdims=True)) / np.std(x, axis=1, keepdims=True)
         x = x * w + b
         return x
 
-    #链接[cls] token的输出层
+    # 链接[cls] token的输出层
     def pooler_output_layer(self, x):
         x = np.dot(x, self.pooler_dense_weight.T) + self.pooler_dense_bias
         x = np.tanh(x)
         return x
 
-    #最终输出
+    # 最终输出
     def forward(self, x):
         x = self.embedding_forward(x)
         sequence_output = self.all_transformer_layer_forward(x)
@@ -204,10 +209,10 @@ class DiyBert:
         return sequence_output, pooler_output
 
 
-#自制
+# 自制
 db = DiyBert(state_dict)
 diy_sequence_output, diy_pooler_output = db.forward(x)
-#torch
+# torch
 torch_sequence_output, torch_pooler_output = bert(torch_x)
 
 print(diy_sequence_output)
