14c6aeaba3cabe842b525ebf85fc34ba110ebb59
DJL1n <djl1n_private@163.com>
2024-05-10 22:50:15 +0800

    2024.5.10

--- 大创项目/申报前的实验准备/中文句子关系推断.py
+++ 大创项目/申报前的实验准备/中文句子关系推断.py
@@ -7,7 +7,7 @@ from datasets import load_dataset
 import random
 
 
-#定义数据集
+# 定义数据集
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, split):
         dataset = load_dataset(path='lansinuote/ChnSentiCorp', split=split)
@@ -23,12 +23,12 @@ class Dataset(torch.utils.data.Dataset):
     def __getitem__(self, i):
         text = self.dataset[i]['text']
 
-        #切分一句话为前半句和后半句
+        # 切分一句话为前半句和后半句
         sentence1 = text[:20]
         sentence2 = text[20:40]
         label = 0
 
-        #有一半的概率把后半句替换为一句无关的话
+        # 有一半的概率把后半句替换为一句无关的话
         if random.randint(0, 1) == 0:
             j = random.randint(0, len(self.dataset) - 1)
             sentence2 = self.dataset[j]['text'][20:40]
@@ -45,14 +45,15 @@ print(len(dataset), sentence1, sentence2, label)
 
 from transformers import BertTokenizer
 
-#加载字典和分词工具
+# 加载字典和分词工具
 token = BertTokenizer.from_pretrained('bert-base-chinese')
+
 
 def collate_fn(data):
     sents = [i[:2] for i in data]
     labels = [i[2] for i in data]
 
-    #编码
+    # 编码
     data = token.batch_encode_plus(batch_text_or_text_pairs=sents,
                                    truncation=True,
                                    padding='max_length',
@@ -61,20 +62,20 @@ def collate_fn(data):
                                    return_length=True,
                                    add_special_tokens=True)
 
-    #input_ids:编码之后的数字
-    #attention_mask:是补零的位置是0,其他位置是1
-    #token_type_ids:第一个句子和特殊符号的位置是0,第二个句子的位置是1
+    # input_ids:编码之后的数字
+    # attention_mask:是补零的位置是0,其他位置是1
+    # token_type_ids:第一个句子和特殊符号的位置是0,第二个句子的位置是1
     input_ids = data['input_ids']
     attention_mask = data['attention_mask']
     token_type_ids = data['token_type_ids']
     labels = torch.LongTensor(labels)
 
-    #print(data['length'], data['length'].max())
+    # print(data['length'], data['length'].max())
 
     return input_ids, attention_mask, token_type_ids, labels
 
 
-#数据加载器
+# 数据加载器
 loader = torch.utils.data.DataLoader(dataset=dataset,
                                      batch_size=8,
                                      collate_fn=collate_fn,
@@ -91,21 +92,22 @@ print(input_ids.shape, attention_mask.shape, token_type_ids.shape, labels)
 
 from transformers import BertModel
 
-#加载预训练模型
+# 加载预训练模型
 pretrained = BertModel.from_pretrained('bert-base-chinese')
 
-#不训练,不需要计算梯度
+# 不训练,不需要计算梯度
 for param in pretrained.parameters():
     param.requires_grad_(False)
 
-#模型试算
+# 模型试算
 out = pretrained(input_ids=input_ids,
-           attention_mask=attention_mask,
-           token_type_ids=token_type_ids)
+                 attention_mask=attention_mask,
+                 token_type_ids=token_type_ids)
 
 print(out.last_hidden_state.shape)
 
-#定义下游任务模型
+
+# 定义下游任务模型
 class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -127,12 +129,12 @@ class Model(torch.nn.Module):
 model = Model()
 
 print(model(input_ids=input_ids,
-      attention_mask=attention_mask,
-      token_type_ids=token_type_ids).shape)
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids).shape)
 
 from transformers import AdamW
 
-#训练
+# 训练
 optimizer = AdamW(model.parameters(), lr=5e-4)
 criterion = torch.nn.CrossEntropyLoss()
 
@@ -156,7 +158,8 @@ for i, (input_ids, attention_mask, token_type_ids,
     if i == 300:
         break
 
-#测试
+
+# 测试
 def test():
     model.eval()
     correct = 0
